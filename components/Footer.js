b95f27d041a83e58be7ab4af61732ef72e3ae2c1
ohizz <jtsamuel3084@gmail.com>
2023-04-24 15:19:40 +0100

    new log

--- components/Footer.js
+++ components/Footer.js
@@ -5,20 +5,20 @@ export const Footer = () => {
 
  return(
   <footer className="mt-10 text-slate-500 border-t border-slate-200 py-3 mt-4 max-w-4xl mx-auto font-body">
-    <div className=" mx-6 md:flex md:justify-between">
-    <div className="mb-4">
+    <div className=" mx-6 flex flex-col md:flex-row md:justify-between">
+    <ul className="mb-4 flex flex-col gap-2 md:flex-row">
     <Link href="#" className="hover:underline">Home</Link>
-    <Link href="#" className="mx-4 hover:underline">Blog</Link>
-    <Link href="#" className="mr-4 hover:underline">Projects</Link>
+    <Link href="#" className="md:mx-4 hover:underline">Blog</Link>
+    <Link href="#" className="md:mr-4 hover:underline">Projects</Link>
     <Link href="#" className= "hover:underline">CV</Link>
-   </div>
-   <div>
-   <Link href="#" className="hover:underline">Twitter</Link>
-    <Link href="#" className="mx-4">Github</Link>
+   </ul>
+   <ul className="flex flex-col md:flex-row gap-2">
+   {/* <Link href="#" className="hover:underline">Twitter</Link> */}
+    <Link href="#" className="md:mx-4">Github</Link>
     <Link href="#" className="hover:underline">Instagram</Link>
-   </div>
+   </ul>
     </div>
-   <p className="text-xs uppercase text-right mr-5 mt-5">© {year} Ohis Samuel</p>
+   <p className="text-sm mx-6 text-left mr-5 mt-5">© {year} Ohis Samuel</p>
   </footer>
  )
 }

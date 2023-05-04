19a3282e07e2d9dea44354b939966b41911bf503
ohizz <jtsamuel3084@gmail.com>
2023-05-04 16:47:14 +0100

    padbottom

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,6 @@
 
 export const Header = () => {
  return(
-    <h1 className='font-ear md:hidden my-8 text-3xl md:text-8xl font-bold text-right tracking-tighter text-right leading-snug'>os.</h1>
+    <h1 className='font-ear md:hidden pt-4 pb-16 text-3xl md:text-8xl font-bold text-right tracking-tighter text-right leading-snug'>os.</h1>
  )
 }

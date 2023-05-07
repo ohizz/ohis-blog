d907711295ca461c216c41c4d4f8b82290c0e97c
ohizz <jtsamuel3084@gmail.com>
2023-05-07 20:07:29 +0100

    sunday

--- src/pages/project.js
+++ src/pages/project.js
@@ -5,24 +5,22 @@ import { Footer } from '../../components/Footer'
 export const Projects = () => {
     return (
     <>
-    <main className="max-w-4xl mx-14 min-h-screen-[100vh] font-body">
+    <main className="max-w-5xl mx-4 md:mx-auto min-h-screen-[100vh] font-body">
     <Header/>
-    <div className=''>
-    <div className='mt-4 flex'>
-    <Link href='#' className=' '>kicks</Link>
-    <span>2023↗</span>
+    <div className='flex justify-between flex-col gap-3 '>
+    <div className='mt-6 pb-4 border-zinc-900 border-b-2 flex'>
+    <Link href='#' className='font-body font-semibold md:tracking-tightest text-4xl md:text-8xl'>kicks</Link>
+
   </div>
-  <div className=''>
-    <Link href='#' className=''>budgettracker</Link>
-    <span>2022↗</span>
+  <div className='my-8 pb-4 border-zinc-900 border-b-2 text-right'>
+    <Link href='#' className='font-body font-semibold text-right md:tracking-tightest text-4xl md:text-8xl'>budgettracker</Link>
   </div>
-  <div className=''>
-    <Link href='#' className=''>todo</Link>
-    <span>2022↗</span>
+  <div className='pb-4 border-zinc-900 border-b-2 '>
+    <Link href='#' className='font-body font-semibold md:tracking-tightest text-4xl md:text-8xl'>todo</Link>
   </div>
     </div>
 
-{/* <div className='text-xs font-lighter'>
+{/* <div className='my-6 text-right text-xs tracking-tight font-lighter'>
   <h3 className='font-semibold text-sm'>stack</h3>
   <p>javascript</p>
   <p>typescript</p>

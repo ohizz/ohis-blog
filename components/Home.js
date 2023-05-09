e4aeb2388e5d5026c3e18dee794ac06942f7ddef
ohizz <jtsamuel3084@gmail.com>
2023-05-09 22:41:01 +0100

    projects

--- components/Home.js
+++ components/Home.js
@@ -6,15 +6,13 @@ export const Home = () => {
  
   return(
  <>
-  <main className=" font-body flex flex-col mx-4 md:mx-16 justify-center min-h-[80vh]">
+  <main className=" font-body flex flex-col mx-4 md:mx-16 justify-center min-h-[70vh]">
     <Header/>
 
   <h1 className="text-3xl mt-6 md:mt-10 font-semibold tracking-tight leading-1 mb-4">Hello there.</h1>
   <p className="text-sm leading-6 md:text-md tracking-tight md:leading-7">Hi I m a frontend developer. My focus is on building dynamic and interactive websites that provide engaging user experiences.</p>
 
-  <p className="text-sm leading-6 md:text-md tracking-tight md:leading-7 my-2">I m skilled in HTML, CSS, and JavaScript, and I m proficient in a variety of frontend frameworks such as React, Vue, and Angular. I have a strong eye for design and understand the importance of creating websites that not only look great but also function seamlessly across all devices.</p>
-
-
+  <p className="text-sm leading-6 md:text-md tracking-tight md:leading-7 my-2">I m skilled in HTML, CSS, and JavaScript, and I m proficient in a variety of frontend frameworks such as React and Nextjs and i also worked with Nodejs and MongoDB. I have a strong eye for design and understand the importance of creating websites that not only look great but also function seamlessly across all devices.</p>
 
 <p className="text-sm leading-6 md:text-md md:leading-7 "> I m passionate about staying up-to-date with the latest trends and technologies in the frontend development field, and I m always looking for new ways to improve my skills and deliver better results.</p>
  </main>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,17 +5,17 @@ const Projects = () => {
     <div className='bg-gradient-to-t from-slate-100/50 from-50% to-slate-100 to-50%'>
     <main className="pt-4 font-body justify-center items-center flex flex-col mx-4  justify-center min-h-[67vh]">
     <div className="flex flex-col flex-wrap">
-    <Link href="#" className="hover:border hover:bg-slate-200 hover:py-1 rounded">
+    <Link href="#" className="hover:border hover:bg-slate-200 hover:p-2 rounded">
         <div className="flex justify-center gap-4 md:gap-60 items-center">
-            <div className="flex justify-start items-start gap-4 items-center"><p className="md:text-3xl tracking-tighter text-black-900">kick</p><small className="text-slate-700 text-xs flex flex-col items-start">react + nodejs <span className="text-slate-500 tracking-tighter md:text-3xl">an ecommerce web app where you can buy sneaker</span></small></div> <p className="text-slate-400">2023</p>
+            <div className="flex justify-start items-start gap-4 items-center"><p className="md:text-3xl tracking-tight md:tracking-tighter text-black-900 w-20 ">kick</p><small className="text-slate-700 text-xs flex flex-col items-start">react + nodejs <span className="text-slate-500 tracking-tight md:tracking-tighter md:text-3xl w-4/5">an ecommerce web app where you can buy sneaker</span></small></div> <p className="text-zinc-800">2023</p>
             </div>
     </Link>
-    <div className="my-8">
-        <div className="flex justify-center md:gap-60 items-center"><div className="flex justify-center gap-4 items-center"><p className="text-slate-700 md:text-3xl tracking-tighter">kick</p><small className="text-slate-700 text-xs flex flex-col items-start">react + nodejs <span className="text-slate-500 tracking-tighter md:text-3xl">an ecommerce web app where you can buy sneaker</span></small></div> <p className="text-slate-400">2023</p></div>
-    </div>
-    <div>
-        <div className="flex justify-center md:gap-60 items-center"><div className="flex justify-center gap-4 items-center"><p className="text-slate-700 md:text-3xl tracking-tighter">kick</p><small className="text-slate-700 text-xs flex flex-col items-start">react + nodejs <span className="text-slate-500 tracking-tighter md:text-3xl">an ecommerce web app where you can buy sneaker</span></small></div> <p className="text-slate-400">2023</p></div>
-    </div>
+    <Link href="#" className="my-8 hover:border hover:bg-slate-200 hover:p-2 rounded">
+        <div className="flex justify-center gap-4 md:gap-60 items-center"><div className="flex justify-center gap-4 items-center"><p className="text-slate-700 md:text-3xl tracking-tight md:tracking-tighter w-20">budget tracker</p><small className="text-slate-700 text-xs flex flex-col items-start">reactjs + context API<span className="text-slate-500 tracking-tight md:tracking-tighter md:text-3xl w-4/5">this app helps you track your expenses</span></small></div> <p className="text-zinc-800">2023</p></div>
+    </Link>
+    <Link href="#" className="hover:border hover:bg-slate-200 hover:p-2 rounded">
+        <div className="flex justify-center gap-4 md:gap-60 items-center"><div className="flex justify-center gap-4 items-center"><p className="text-slate-700 md:text-3xl tracking-tight md:tracking-tighter w-20">todo</p><small className="text-slate-700 text-xs flex flex-col items-start">reactjs <span className="w-4/5 text-slate-500 tracking-tight md:tracking-tighter md:text-3xl">a web app that helps you iterate what you want to do in your daily activities</span></small></div> <p className="text-zinc-800">2023</p></div>
+    </Link>
     </div>
     </main>
     <Footer/>

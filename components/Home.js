a27e97d865391e51c6053908b5815dd9bcd5bd3b
ohizz <jtsamuel3084@gmail.com>
2023-05-11 11:08:42 +0100

    newh

--- components/Home.js
+++ components/Home.js
@@ -2,9 +2,13 @@ import  {Footer} from "./Footer"
 import { Header } from "./Header"
 import Link from "next/link"
 import { motion } from "framer-motion"
+import Ticker from 'framer-motion-ticker';
+import React from 'react';
 
 export const Home = () => {
- 
+  const [isPlaying, setIsPlaying] = React.useState(true)
+  const skills = ['rectjs', 'tailwindcss', 'nodejs', 'nextjs', 'javascript'];
+
   return(
  <>
   {/* <Header/> */}
@@ -29,21 +33,36 @@ export const Home = () => {
 
 <p className="text-sm leading-6 md:text-md md:leading-7 mt-4"> I m passionate about staying up-to-date with the latest trends and technologies in the frontend development field, and I m always looking for new ways to improve my skills and deliver better results.</p>
 </div>
+
+<Ticker duration={5} onMouseEnter={() => setIsPlaying(false)} onMouseLeave={() => setIsPlaying(true)} isPlaying={isPlaying}>
+        {skills.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              position: 'relative',
+              content: 'ohis',
+              width: '50px',
+              height: '70px',
+              margin: '5px',
+            }}
+          />
+        ))}
+      </Ticker>
 <div className="my-10 flex flex-col flex-wrap">
   <h2 className="mb-4 tracking-tighter text-md text-zinc-400 border-b border-zinc-500 pb-2">projects</h2>
  <div className="flex flex-col gap-4">
         <div className="flex justify-center gap-4 items-center">
             <div className="flex justify-start items-start gap-4 items-center basis-full">
-              <p className="text-xs text-zinc-200 flex flex-wrap gap-1 flex-col items-start flex-2">kick</p>
+              <p className="text-xs text-zinc-200 flex flex-wrap gap-1 flex-col items-start flex-2">kick <span className="border border-zinc-600 text-ds rounded px-2 py-1 ">ongoing</span> </p>
               <div className="text-zinc-400 text-ds flex flex-col items-start flex-5 gap-2">
-                <div className="flex gap-3 items-center">
-                <small>react + nodejs</small> <small>2023</small> <span className="border border-zinc-600 text-ds rounded px-2 py-1 ml-3 ">ongoing</span> 
+                <div className="flex gap-3 items-center flex-wrap">
+                <small>react + nodejs</small> <small>2023</small> 
                 </div>
                 <p>an ecommerce web app where you can buy sneaker</p>
                 </div>
                 </div>  
                 
-              <div className="text-zinc-200 text-ds flex gap-2">
+              <div className="text-zinc-200 text-ds flex flex-wrap md:flex-nowrap gap-2">
                 <Link href="#" className="border-2 border-zinc-700 py-1 px-2 rounded-full">github</Link><Link href="#" className="border-2 border-zinc-700 py-1 px-2 rounded-full">live</Link></div>
             </div>
     <div className="flex justify-center gap-4 items-center">
@@ -56,7 +75,7 @@ export const Home = () => {
                 <p>track expenses</p>
                 </div>
                 </div>  
-              <div className="text-zinc-200 text-ds flex gap-2">
+              <div className="text-zinc-200 text-ds flex gap-2 flex-wrap md:flex-nowrap ">
                 <Link className="border-2 border-zinc-700 py-1 px-2 rounded-full" href="#">github</Link><Link href="#" className="border-2 border-zinc-700 py-1 px-2 rounded-full">live</Link></div>
             </div>
     <div className="flex justify-center gap-4 items-center">
@@ -69,7 +88,7 @@ export const Home = () => {
                 <p>itemized all your work</p>
                 </div>
                 </div>  
-              <div className="text-zinc-200 text-ds flex gap-2">
+              <div className="text-zinc-200 text-ds flex gap-2 flex-wrap md:flex-nowrap ">
                 <Link href="#" className="border-2 border-zinc-700 py-1 px-2 rounded-full">github</Link><Link href="#" className="border-2 border-zinc-700 py-1 px-2 rounded-full">live</Link></div>
             </div>
     </div>
